2e53bcbb6d135fdc3afc5ceab8f5854b1557676b
Sam <sam@Grayson-Perry.local>
2022-10-26 09:58:43 +0100

    logging to debug 304 error

--- handlers/getPlaces.js
+++ handlers/getPlaces.js
@@ -12,6 +12,6 @@ export default async (req, res) => {
     typeof parseFloat(req.params.lng) === "number" ? req.params.lng : -98.5795;
   const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?location=${lat}%2C${lng}&radius=100000&types=establishment&input=${query}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   const places = await fetch(url).then((data) => data.json());
-
+  console.log(places);
   res.status(200).json(places);
 };
