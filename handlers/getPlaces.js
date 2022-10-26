cf2864c94a1059a51ad62eafd2348f090a320d82
Sam <sam@Grayson-Perry.local>
2022-10-26 16:42:46 +0100

    region added to autocomplete

--- handlers/getPlaces.js
+++ handlers/getPlaces.js
@@ -10,7 +10,7 @@ export default async (req, res) => {
     typeof parseFloat(req.params.lat) === "number" ? req.params.lat : 39.8283;
   const lng =
     typeof parseFloat(req.params.lng) === "number" ? req.params.lng : -98.5795;
-  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?location=${lat}%2C${lng}&radius=100000&types=establishment&input=${query}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?location=${lat}%2C${lng}&region=us&radius=100000&types=establishment&input=${query}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   const places = await fetch(url).then((data) => data.json());
   console.log(places);
   res.status(200).json(places);
